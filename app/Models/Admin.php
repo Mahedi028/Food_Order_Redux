2b2019f8444994739a5dd6298d51b9caf00206d6
mahedihasan <programmermahedi347@gmail.com>
2023-04-10 01:16:07 +0600

    passport-setup

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -8,10 +8,9 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
-// use Laravel\Sanctum\HasApiTokens;
-use Laravel\Passport\HasApiTokens;
+use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class Admin extends Authenticatable
 {
     use HasApiTokens;
     use HasFactory;
